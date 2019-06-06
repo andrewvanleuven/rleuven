e57030180e047d4fc41ac61e20b10fc7ec89c1dc
Andrew Van Leuven <a@andrewvanleuven.com>
2019-06-06 19:24:43 -0400

    commit

--- R/freqTab.R
+++ R/freqTab.R
@@ -1,11 +1,11 @@
 freqTab <- function(df,var) {
   dfs <- df %>% select(var)
   dft <- table(dfs)
-  as.data.frame(dft) %>% 
+  as.data.frame(dft) %>%
     mutate(p = round((Freq/(sum(Freq)/100)), digits=1))%>%
     mutate(rank = rank(desc(p))) %>%
     filter(rank < 16) %>%
     arrange(desc(Freq)) %>%
     select(-rank) %>%
-    `colnames<-`(c(var,"N","Percent"))
+    `colnames<-`(c(var,"Observations","Percent"))
 }
