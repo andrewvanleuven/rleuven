74c4724e7b4df6b0c32c112e68e3b97be20f153b
Andrew Van Leuven <a@andrewvanleuven.com>
2019-06-06 20:17:59 -0400

    Improved functionality of freqTab function

--- R/freqTab.R
+++ R/freqTab.R
@@ -1,11 +1,17 @@
-freqTab <- function(df,var) {
-  dfs <- df %>% select(var)
+freqTab <- function(df,var,maxvals) {
+  require("dplyr")
+  if(missing(maxvals)) {
+    maxvals=10
+  }
+  maxv <- (maxvals + 1)
+  dfs <- as_tibble(df) %>% select(var)
   dft <- table(dfs)
+  varname <- str_to_upper(var,locale = "en")
   as.data.frame(dft) %>%
     mutate(p = round((Freq/(sum(Freq)/100)), digits=1))%>%
     mutate(rank = rank(desc(p))) %>%
-    filter(rank < 16) %>%
+    filter(rank < maxv) %>%
     arrange(desc(Freq)) %>%
     select(-rank) %>%
-    `colnames<-`(c(var,"N","Percent"))
+    `colnames<-`(c(varname,"N","Percent"))
 }
