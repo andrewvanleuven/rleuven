cbf37329352e8c5e88916234b7cbeff7a1e4a964
Andrew Van Leuven <a@andrewvanleuven.com>
2019-08-23 09:29:08 -0400

    Fixed Ohio data

---
+++ data-raw/ohio.R
@@ -0,0 +1,15 @@
+## code to prepare `ohio` dataset goes here
+
+library(tidyverse)
+
+ohio <- read_csv('data-raw/ohio.csv') %>%
+  filter(place != 99990 & place != 0 & place_pop_2010 != "A") %>%
+  mutate(place_name = str_replace_all(place_name, "\\(pt.\\)","")) %>%
+  group_by(place_name) %>%
+  top_n(1, place_pop_2010) %>%
+  mutate(place_pop_2010 = as.numeric(place_pop_2010)) %>%
+  filter(place_pop_2010 > 75 & !is.na(place_pop_2010)) %>%
+  distinct() %>%
+  arrange(county_name,place_name)
+
+usethis::use_data(ohio,overwrite = T)
