540f0e6434aa6bb208138d53ea79273beb891c9c
Andrew Van Leuven <a@andrewvanleuven.com>
2019-09-02 01:03:18 -0400

    fixed random colors

--- R/rand_ncolors.R
+++ R/rand_ncolors.R
@@ -1,6 +1,6 @@
 rand_ncolors <- function(df){
   n <- nrow(df)
   df <- inlmisc::GetColors(n)
+  message("Use `scale_fill_manual(values = n_colors)` for random palette.")
   sample(df)
-  print("Use `scale_fill_manual(values = n_colors)` for random palette.")
 }
