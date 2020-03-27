32866131f8e2233084a2f6ab90aaa5c9eee3dec1
Andrew J. Van Leuven <33671616+andrewvanleuven@users.noreply.github.com>
2020-03-27 00:47:26 -0400

    added ncaa_hex_select function to grab hex codes directly

---
+++ R/ncaa_hex_select.R
@@ -0,0 +1,18 @@
+ncaa_hex_select <- function(university,row_number,secondary=c('TRUE','FALSE')){
+  df <- college_hex
+  require("tidyverse")
+  if(missing(secondary)) {
+    secondary=F
+  }
+  search_key <- str_to_lower(university)
+  first_run <- df %>%
+    filter(str_detect(hex_color,search_key))
+  if (secondary==FALSE) return({first_run %>%
+      filter(str_detect(hex_color,"_1")) %>%
+      slice(row_number) %>%
+      pull(value)})
+  if (secondary==TRUE) return({first_run %>%
+      filter(str_detect(hex_color,"_2")) %>%
+      slice(row_number) %>%
+      pull(value)})
+}
